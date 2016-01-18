4645b60ef646f93e5427033182903078f4d9626b
root <root@localhost.localdomain>
2016-01-18 00:05:50 -0500

    initial

---
+++ myPython/myTest1.py
@@ -0,0 +1,11 @@
+def Mod2(i):
+	if i%2 :
+		print "It's ODD"
+	else:
+		print "It's EVEN"
+
+something=raw_input("Type I Valuse : ")
+i=something
+j=int(i)+1
+print  'The First Valuse is %s\nThe Second Valuse is %s' %(i,j)
+Mod2(i)
---
+++ myPython/test.py
@@ -0,0 +1,1 @@
+print "Hello, Jaemi Lee"
